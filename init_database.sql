7eb394158983b6b862c3829d211b2c8d26a8c0ab
Ahmad Khoirunnufus <a.khoirunnufus@gmail.com>
2023-10-21 14:31:04 +0700

    create init_database.sql file

---
+++ init_database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tbl_pegawai (
+    id_pegawai INT(11) AUTO_INCREMENT PRIMARY KEY,
+    nama_pegawai VARCHAR(50),
+    username VARCHAR(20),
+    password TEXT,
+    email VARCHAR(50),
+    nohp VARCHAR(20)
+)
